9818cc266661416e865bc7681b79d5500556a051
Jean-Philippe Gelinas <jpgelinas@avencall.com>
2014-01-24 14:59:06 -0500

    add xivo-call-logd daemon skeleton

--- xivo-call-logs/xivo_call_logs/bin/__init__.py
+++ xivo-call-logs/xivo_call_logs/bin/__init__.py
@@ -1,7 +1,6 @@
-#!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-# Copyright (C) 2012-2013  Avencall
+# Copyright (C) 2013-2014 Avencall
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -15,7 +14,3 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>
-
-from xivo_call_logs.bin.main import main
-
-main()
