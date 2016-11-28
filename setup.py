50a58d5bad676a921e511b5b59fc44862ace45c7
Pascal Cadotte Michaud <pascal.cadotte@gmail.com>
2016-11-28 11:20:38 -0500

    setup.py: xivo-pbx -> wazo-pbx

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setup(
     description='XiVO Call Logs Generation',
     author='Avencall',
     author_email='dev@avencall.com',
-    url='https://github.com/xivo-pbx/xivo-call-logs',
+    url='https://github.com/wazo-pbx/xivo-call-logs',
     license='GPLv3',
     packages=find_packages(),
     scripts=['bin/xivo-call-logs', 'bin/xivo-call-logd'],
