6b5483c24642df94f6355306154cbacea71ee992
Sebastien Duthil <dev.wazo+git@gmail.com>
2017-01-05 09:55:12 -0500

    local originate interpretor: gracefully reject invalid cel block
    reason: in some cases where the cel block would only contain one
    CHAN_START, can_interpret() would raise an exception, instead of
    returning false

--- xivo_call_logs/cel_interpretor.py
+++ xivo_call_logs/cel_interpretor.py
@@ -199,11 +199,13 @@ class LocalOriginateCELInterpretor(object):
     @classmethod
     def first_two_channels_are_local(cls, cels):
         names = [cel.channame for cel in cels if cel.eventtype == 'CHAN_START']
-        return (names[0].lower().startswith('local/') and
+        return (len(names) >= 2 and
+                names[0].lower().startswith('local/') and
                 names[1].lower().startswith('local/'))
 
     @classmethod
     def first_channel_is_answered_before_any_other_operation(cls, cels):
         first_channel_cels = [cel for cel in cels if cel.uniqueid == cels[0].uniqueid]
-        return (first_channel_cels[0].eventtype == 'CHAN_START' and
+        return (len(first_channel_cels) >= 2 and
+                first_channel_cels[0].eventtype == 'CHAN_START' and
                 first_channel_cels[1].eventtype == 'ANSWER')
