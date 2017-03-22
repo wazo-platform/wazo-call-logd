18b43f7b6a5776db0190c21f6aab740d8efc13c4
Sebastien Duthil <sduthil@wazo.community>
2017-03-22 14:03:33 -0400

    tests: create call_logd client

--- integration_tests/suite/test_cdr.py
+++ integration_tests/suite/test_cdr.py
@@ -2,13 +2,12 @@
 # Copyright 2017 The Wazo Authors  (see AUTHORS file)
 # SPDX-License-Identifier: GPL-3.0+
 
-import requests
-
 from hamcrest import assert_that
 from hamcrest import equal_to
 from hamcrest import contains_string
 
 from .test_api.base import IntegrationTest
+from .test_api.call_logd import CallLogdClient
 
 
 class TestNoAuth(IntegrationTest):
@@ -16,14 +15,16 @@ class TestNoAuth(IntegrationTest):
     asset = 'base'
 
     def test_given_no_auth_when_list_cdr_then_503(self):
+        ctid_ng = CallLogdClient('localhost', 9298)
         with self.auth_stopped():
-            result = requests.get('https://localhost:9298/1.0/cdr', verify=False)
+            result = ctid_ng.get_cdr_result()
 
             assert_that(result.status_code, equal_to(503))
             assert_that(result.json()['message'].lower(), contains_string('auth'))
 
     def test_given_no_token_when_list_cdr_then_401(self):
-        result = requests.get('https://localhost:9298/1.0/cdr', verify=False)
+        ctid_ng = CallLogdClient('localhost', 9298)
+        result = ctid_ng.get_cdr_result()
 
         assert_that(result.status_code, equal_to(401))
         assert_that(result.json()['message'].lower(), contains_string('unauthorized'))
