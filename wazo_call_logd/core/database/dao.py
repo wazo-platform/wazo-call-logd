187f216ca1f1dffab8d92d13e28f991c5f57de70
Anthony Delaby <pro@anthonydelaby.me>
2017-07-13 14:14:21 +0200

    Replace `make_transient` by `session.expunge_all

--- wazo_call_logd/core/database/dao.py
+++ wazo_call_logd/core/database/dao.py
@@ -13,7 +13,6 @@ from sqlalchemy.dialects.postgresql import ARRAY
 from sqlalchemy.orm import joinedload
 from sqlalchemy.orm import scoped_session
 from sqlalchemy.orm import sessionmaker
-from sqlalchemy.orm.session import make_transient
 from sqlalchemy.pool import Pool
 
 from xivo_dao.alchemy.call_log import CallLog as CallLogSchema
@@ -76,9 +75,7 @@ class CallLogDAO(object):
         with self.new_session() as session:
             cdr = session.query(CallLogSchema).options(joinedload('participants')).get(cdr_id)
             if cdr:
-                make_transient(cdr)
-                for participant in cdr.participants:
-                    make_transient(participant)
+                session.expunge_all()
                 return cdr
 
     def find_all_in_period(self, params):
@@ -138,9 +135,7 @@ class CallLogDAO(object):
             if not call_log_rows:
                 return []
             for call_log in call_log_rows:
-                make_transient(call_log)
-                for participant in call_log.participants:
-                    make_transient(participant)
+                session.expunge_all()
 
             return call_log_rows
 
