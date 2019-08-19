76976f00bfb1b5a5ac6ac717b993983a2bce7fc1
Pascal Cadotte Michaud <pcm@wazo.io>
2019-08-19 10:20:01 -0400

    tests: add a test class to exercices the dao queries
    this is a "unittest" for the database queries but is in the integration test
    directory since it needs a Postgresql to execute

--- integration_tests/suite/test_database.py
+++ integration_tests/suite/test_database.py
@@ -2,16 +2,34 @@
 # SPDX-License-Identifier: GPL-3.0-or-later
 
 import logging
+import os
 
-from hamcrest import assert_that
-from hamcrest import empty
-from hamcrest import has_entry
+from hamcrest import (
+    assert_that,
+    empty,
+    has_entry,
+    not_,
+)
 
 from xivo_test_helpers import until
+from xivo_test_helpers.asset_launching_test_case import (
+    AssetLaunchingTestCase,
+    NoSuchService,
+    NoSuchPort,
+)
 
-from .helpers.base import IntegrationTest
+from wazo_call_logd.database import dao
+
+from .helpers.base import (
+    DbHelper,
+    IntegrationTest,
+    WrongClient,
+)
+from .helpers.database import call_logs
+from .helpers.constants import MASTER_TENANT
 
 logger = logging.getLogger(__name__)
+DB_URI = os.getenv('DB_URI', 'postgresql://asterisk:proformatique@localhost:{port}')
 
 
 class TestDatabase(IntegrationTest):
@@ -33,3 +51,67 @@ class TestDatabase(IntegrationTest):
 
         assert_that(result1, has_entry('items', empty()))
         assert_that(result2, has_entry('items', empty()))
+
+
+class TestDAO(AssetLaunchingTestCase):
+
+    asset = 'database'
+    assets_root = os.path.join(os.path.dirname(__file__), '..', 'assets')
+    service = 'postgres'
+
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        try:
+            cls.database = DbHelper.build(
+                'asterisk',
+                'proformatique',
+                'localhost',
+                cls.service_port(5432, 'postgres'),
+                'asterisk',
+            )
+        except (NoSuchService, NoSuchPort) as e:
+            logger.debug(e)
+            cls.database = WrongClient(name='database')
+
+    def setUp(self):
+        db_uri = DB_URI.format(port=self.service_port(5432, 'postgres'))
+        Session = dao.new_db_session(db_uri)
+        self.dao = dao.CallLogDAO(Session)
+
+    @call_logs(
+        [
+            {
+                'id': 12,
+                'date': '2017-03-23 00:00:00',
+                'date_answer': '2017-03-23 00:01:00',
+                'date_end': '2017-03-23 00:02:27',
+                'destination_exten': '3378',
+                'destination_name': 'dést,ination',
+                'destination_internal_exten': '3245',
+                'destination_internal_context': 'internal',
+                'direction': 'internal',
+                'requested_exten': '3958',
+                'requested_internal_exten': '3490',
+                'requested_internal_context': 'internal',
+                'source_exten': '7687',
+                'source_name': 'soùr.',
+                'source_internal_exten': '5938',
+                'source_internal_context': 'internal',
+                'participants': [
+                    {
+                        'user_uuid': 42,
+                        'line_id': '11',
+                        'tags': ['rh', 'Poudlard'],
+                        'role': 'source',
+                    },
+                    {'user_uuid': 43, 'line_id': '22', 'role': 'destination'},
+                ],
+            }
+        ]
+    )
+    def test_get_by_id(self):
+        # TODO remove this test it was added to test the DB test boilerplate
+        result = self.dao.get_by_id(12, [MASTER_TENANT])
+
+        assert_that(result, not_(None))
