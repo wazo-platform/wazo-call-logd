18533e1f680f9257d83d2ec96b70ef86eed5cad2
Alexandre Fournier <afournier@wazo.io>
2020-11-24 08:56:37 -0500

    support_center: fix datetimes in schema

--- wazo_call_logd/plugins/support_center/schema.py
+++ wazo_call_logd/plugins/support_center/schema.py
@@ -1,15 +1,17 @@
 # Copyright 2020 The Wazo Authors  (see the AUTHORS file)
 # SPDX-License-Identifier: GPL-3.0-or-later
 
+import pytz
+
 from marshmallow import (
     fields,
     post_load,
+    pre_dump,
     pre_load,
     validates_schema,
     ValidationError,
 )
-from datetime import timezone, time
-from pytz import all_timezones
+from datetime import time
 from marshmallow.validate import OneOf, ContainsOnly, Range, Regexp
 from xivo.mallow_helpers import Schema
 
@@ -35,8 +37,8 @@ class AgentStatisticsSchemaList(Schema):
 
 
 class QueueStatisticsSchema(Schema):
-    from_ = fields.DateTime(attribute='from', data_key='from')
-    until = fields.DateTime()
+    from_ = fields.String(attribute='from', data_key='from')
+    until = fields.String()
     tenant_uuid = fields.UUID(default=None)
     queue_id = fields.Integer(default=None)
     queue_name = fields.String(default=None)
@@ -51,6 +53,14 @@ class QueueStatisticsSchema(Schema):
     answered_rate = fields.Float(default=None)
     quality_of_service = fields.Float(attribute='qos', default=None)
 
+    @pre_dump
+    def convert_from_and_until_to_isoformat(self, data, **kwargs):
+        if data.get('from'):
+            data['from'] = data['from'].isoformat()
+        if data.get('until'):
+            data['until'] = data['until'].isoformat()
+        return data
+
 
 class _StatisticsListRequestSchema(Schema):
     from_ = fields.DateTime(data_key='from', missing=None)
@@ -63,7 +73,14 @@ class _StatisticsListRequestSchema(Schema):
         missing=[1, 2, 3, 4, 5, 6, 7],
         validate=ContainsOnly([1, 2, 3, 4, 5, 6, 7]),
     )
-    timezone = fields.String(validate=OneOf(all_timezones), missing='UTC')
+    timezone = fields.String(validate=OneOf(pytz.all_timezones), missing='UTC')
+
+    def _normalize_datetime(self, dt, timezone):
+        if not dt.tzinfo:
+            return timezone.normalize(timezone.localize(dt))
+        else:
+            utc_dt = pytz.utc.normalize(dt)
+            return timezone.normalize(utc_dt)
 
     @pre_load
     def convert_week_days_to_list(self, data):
@@ -84,22 +101,24 @@ class _StatisticsListRequestSchema(Schema):
 
     @post_load
     def default_timezone_on_datetime(self, data, **kwargs):
-        if data.get('from_'):
-            if not data['from_'].tzinfo:
-                data['from_'] = data['from_'].replace(tzinfo=timezone.utc)
-        if data.get('until'):
-            if not data['until'].tzinfo:
-                data['until'] = data['until'].replace(tzinfo=timezone.utc)
+        timezone = pytz.timezone(data.get('timezone'))
+        from_ = data.get('from_')
+        until = data.get('until')
+        if from_:
+            data['from_'] = self._normalize_datetime(from_, timezone)
+        if until:
+            data['until'] = self._normalize_datetime(until, timezone)
         return data
 
     @validates_schema
     def validate_dates(self, data, **kwargs):
         from_ = data.get('from_', None)
         until = data.get('until', None)
-        if from_ and not from_.tzinfo:
-            from_ = from_.replace(tzinfo=timezone.utc)
-        if until and not until.tzinfo:
-            until = until.replace(tzinfo=timezone.utc)
+        timezone = pytz.timezone(data.get('timezone'))
+        if from_:
+            from_ = self._normalize_datetime(from_, timezone)
+        if until:
+            until = self._normalize_datetime(until, timezone)
         if from_ and until and until <= from_:
             raise ValidationError({'until': 'Field must be greater than from'})
 
