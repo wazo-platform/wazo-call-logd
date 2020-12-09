c245c6694aa17961813006f633317becda2035f0
Alexandre Fournier <afournier@wazo.io>
2020-12-09 10:54:17 -0500

    support_center agents: add more timezone tests

--- integration_tests/suite/test_agent_stats.py
+++ integration_tests/suite/test_agent_stats.py
@@ -1,7 +1,9 @@
 # Copyright 2020 The Wazo Authors  (see the AUTHORS file)
 # SPDX-License-Identifier: GPL-3.0-or-later
 
-from datetime import datetime, timedelta, timezone
+import pytz
+
+from datetime import datetime, timedelta
 from dateutil.relativedelta import relativedelta
 
 from hamcrest import (
@@ -207,11 +209,13 @@ class TestStatistics(IntegrationTest):
 
     asset = 'base'
 
-    def _get_tomorrow(self):
-        today = datetime.now(tz=timezone.utc)
-        return (
-            datetime(today.year, today.month, today.day, tzinfo=timezone.utc)
-            + relativedelta(days=1)
+    def _get_tomorrow(self, timezone=None):
+        timezone = timezone or pytz.utc
+        today = timezone.normalize(timezone.localize(datetime.now()))
+        return timezone.normalize(
+            timezone.localize(
+                datetime(today.year, today.month, today.day) + relativedelta(days=1)
+            )
         ).isoformat(timespec='seconds')
 
     def test_list_agent_statistics_when_no_stats(self):
@@ -253,6 +257,49 @@ class TestStatistics(IntegrationTest):
                     has_entries(
                         **{'from': '2020-10-06T13:00:00+00:00'},
                         until=self._get_tomorrow(),
+                        tenant_uuid=MASTER_TENANT,
+                        agent_id=10,
+                        agent_number='1002',
+                        conversation_time=timedelta(seconds=14).total_seconds(),
+                        login_time=timedelta(hours=1).total_seconds(),
+                        pause_time=timedelta(minutes=8).total_seconds(),
+                        wrapup_time=timedelta(minutes=10).total_seconds(),
+                    ),
+                ),
+                total=equal_to(2),
+            ),
+        )
+
+    # fmt: off
+    @stat_agent({'id': 1, 'name': 'Agent/1001', 'agent_id': 42})
+    @stat_agent({'id': 2, 'name': 'Agent/1002', 'agent_id': 10})
+    @stat_call_on_queue({'agent_id': 1, 'time': '2020-10-05 13:05:00', 'talktime': 11, 'status': 'answered'})
+    @stat_call_on_queue({'agent_id': 1, 'time': '2020-10-05 13:25:31', 'talktime': 12, 'status': 'answered'})
+    @stat_agent_periodic({'agent_id': 1, 'time': '2020-10-05 13:00:00', 'login_time': '00:55:00', 'pause_time': '00:15:00', 'wrapup_time': '00:02:00'})
+    @stat_call_on_queue({'agent_id': 2, 'time': '2020-10-06 13:59:00', 'talktime': 14, 'status': 'answered'})
+    @stat_agent_periodic({'agent_id': 2, 'time': '2020-10-06 13:00:00', 'login_time': '01:00:00', 'pause_time': '00:08:00', 'wrapup_time': '00:10:00'})
+    # fmt: on
+    def test_list_agent_statistics_when_no_param_except_timezone_with_stats(self):
+        results = self.call_logd.agent_statistics.list(timezone='America/Montreal')
+        timezone = pytz.timezone('America/Montreal')
+        assert_that(
+            results,
+            has_entries(
+                items=has_items(
+                    has_entries(
+                        **{'from': '2020-10-05T09:00:00-04:00'},
+                        until=self._get_tomorrow(timezone),
+                        tenant_uuid=MASTER_TENANT,
+                        agent_id=42,
+                        agent_number='1001',
+                        conversation_time=timedelta(seconds=11 + 12).total_seconds(),
+                        login_time=timedelta(minutes=55).total_seconds(),
+                        pause_time=timedelta(minutes=15).total_seconds(),
+                        wrapup_time=timedelta(minutes=2).total_seconds(),
+                    ),
+                    has_entries(
+                        **{'from': '2020-10-06T09:00:00-04:00'},
+                        until=self._get_tomorrow(timezone),
                         tenant_uuid=MASTER_TENANT,
                         agent_id=10,
                         agent_number='1002',
@@ -869,6 +916,179 @@ class TestStatistics(IntegrationTest):
                     **{'from': '2020-01-01T00:00:00+00:00'},
                     until='2020-04-01T00:00:00+00:00',
                     login_time=timedelta(minutes=56 + 57).total_seconds(),
+                ),
+            ),
+        )
+
+    # fmt: off
+    @stat_agent({'id': 1, 'name': 'Agent/1001', 'agent_id': 42})
+    @stat_agent_periodic({'agent_id': 1, 'time': '2020-01-01T06:00:00-05:00', 'login_time': '00:55:00'})  # not counted
+    @stat_agent_periodic({'agent_id': 1, 'time': '2020-02-12T10:00:00-05:00', 'login_time': '00:56:00'})  # should be counted
+    @stat_agent_periodic({'agent_id': 1, 'time': '2020-03-31T15:00:00-04:00', 'login_time': '00:57:00'})  # should be counted
+    @stat_agent_periodic({'agent_id': 1, 'time': '2020-03-31T18:00:00-04:00', 'login_time': '00:58:00'})  # not counted
+    # fmt: on
+    def test_monthly_interval_multiple_months_with_all_filters_and_timezone(self):
+        results = self.call_logd.agent_statistics.get_by_id(
+            agent_id=42,
+            from_='2020-01-01T00:00:00',
+            until='2020-04-01T00:00:00',
+            interval='month',
+            day_start_time='08:00',
+            day_end_time='17:00',
+            week_days='1,2,3',
+            timezone='America/Montreal',
+        )
+
+        assert_that(results, has_entries(total=equal_to(4)))
+        common_fields = {
+            'tenant_uuid': MASTER_TENANT,
+            'agent_id': 42,
+            'agent_number': '1001',
+        }
+        assert_that(
+            results['items'],
+            has_items(
+                has_entries(
+                    **common_fields,
+                    **{'from': '2020-01-01T00:00:00-05:00'},
+                    until='2020-02-01T00:00:00-05:00',
+                    login_time=0,
+                ),
+                has_entries(
+                    **common_fields,
+                    **{'from': '2020-02-01T00:00:00-05:00'},
+                    until='2020-03-01T00:00:00-05:00',
+                    login_time=timedelta(minutes=56).total_seconds(),
+                ),
+                has_entries(
+                    **common_fields,
+                    **{'from': '2020-03-01T00:00:00-05:00'},
+                    until='2020-04-01T00:00:00-04:00',
+                    login_time=timedelta(minutes=57).total_seconds(),
+                ),
+                has_entries(
+                    **common_fields,
+                    **{'from': '2020-01-01T00:00:00-05:00'},
+                    until='2020-04-01T00:00:00-04:00',
+                    login_time=timedelta(minutes=56 + 57).total_seconds(),
+                ),
+            ),
+        )
+
+    # fmt: off
+    @stat_agent({'id': 1, 'name': 'Agent/1001', 'agent_id': 42})
+    @stat_agent_periodic({'agent_id': 1, 'time': '2020-10-31T17:00:00-04:00', 'login_time': '00:50:00'})  # not counted
+    @stat_agent_periodic({'agent_id': 1, 'time': '2020-10-31T18:00:00-04:00', 'login_time': '00:51:00'})  # not counted
+    @stat_agent_periodic({'agent_id': 1, 'time': '2020-11-01T08:00:00-05:00', 'login_time': '00:52:00'})  # counted
+    @stat_agent_periodic({'agent_id': 1, 'time': '2020-11-01T16:00:00-05:00', 'login_time': '00:53:00'})  # counted
+    @stat_agent_periodic({'agent_id': 1, 'time': '2020-11-02T7:00:00-05:00', 'login_time': '00:54:00'})  # not counted
+    @stat_agent_periodic({'agent_id': 1, 'time': '2020-11-02T8:00:00-05:00', 'login_time': '00:55:00'})  # counted
+    @stat_agent_periodic({'agent_id': 1, 'time': '2020-11-02T16:00:00-05:00', 'login_time': '00:56:00'})  # counted
+    @stat_agent_periodic({'agent_id': 1, 'time': '2020-11-02T18:00:00-05:00', 'login_time': '00:57:00'})  # not counted
+    @stat_agent_periodic({'agent_id': 1, 'time': '2020-11-03T16:00:00-05:00', 'login_time': '00:58:00'})  # not counted
+    # fmt: on
+    def test_monthly_interval_multiple_days_with_all_filters_and_timezone(self):
+        results = self.call_logd.agent_statistics.get_by_id(
+            agent_id=42,
+            from_='2020-10-31T00:00:00-04:00',
+            until='2020-11-03T00:00:00-05:00',
+            interval='day',
+            day_start_time='08:00',
+            day_end_time='17:00',
+            week_days='1,2,3,4,5,7',
+            timezone='America/Montreal',
+        )
+
+        assert_that(results, has_entries(total=equal_to(3)))
+        common_fields = {
+            'tenant_uuid': MASTER_TENANT,
+            'agent_id': 42,
+            'agent_number': '1001',
+        }
+        assert_that(
+            results['items'],
+            has_items(
+                has_entries(
+                    **common_fields,
+                    **{'from': '2020-11-01T00:00:00-04:00'},
+                    until='2020-11-02T00:00:00-05:00',
+                    login_time=timedelta(minutes=52 + 53).total_seconds(),
+                ),
+                has_entries(
+                    **common_fields,
+                    **{'from': '2020-11-02T00:00:00-05:00'},
+                    until='2020-11-03T00:00:00-05:00',
+                    login_time=timedelta(minutes=55 + 56).total_seconds(),
+                ),
+                has_entries(
+                    **common_fields,
+                    **{'from': '2020-10-31T00:00:00-04:00'},
+                    until='2020-11-03T00:00:00-05:00',
+                    login_time=timedelta(minutes=52 + 53 + 55 + 56).total_seconds(),
+                ),
+            ),
+        )
+
+    # fmt: off
+    @stat_agent({'id': 1, 'name': 'Agent/1001', 'agent_id': 42})
+    @stat_agent_periodic({'agent_id': 1, 'time': '2020-10-31T17:00:00-04:00', 'login_time': '00:50:00'})  # not counted
+    @stat_agent_periodic({'agent_id': 1, 'time': '2020-10-31T18:00:00-04:00', 'login_time': '00:51:00'})  # not counted
+    @stat_agent_periodic({'agent_id': 1, 'time': '2020-11-01T00:00:00-04:00', 'login_time': '00:52:00'})  # counted
+    @stat_agent_periodic({'agent_id': 1, 'time': '2020-11-01T01:00:00-04:00', 'login_time': '00:53:00'})  # counted
+    @stat_agent_periodic({'agent_id': 1, 'time': '2020-11-01T01:00:00-05:00', 'login_time': '00:54:00'})  # counted
+    @stat_agent_periodic({'agent_id': 1, 'time': '2020-11-01T02:00:00-05:00', 'login_time': '00:55:00'})  # counted
+    @stat_agent_periodic({'agent_id': 1, 'time': '2020-11-01T03:00:00-05:00', 'login_time': '00:56:00'})  # counted
+    @stat_agent_periodic({'agent_id': 1, 'time': '2020-11-01T04:00:00-05:00', 'login_time': '00:57:00'})  # not counted
+    @stat_agent_periodic({'agent_id': 1, 'time': '2020-11-02T02:00:00-05:00', 'login_time': '00:58:00'})  # not counted
+    # fmt: on
+    def test_monthly_interval_multiple_hours_with_all_filters_and_timezone_dst_change(
+        self,
+    ):
+        results = self.call_logd.agent_statistics.get_by_id(
+            agent_id=42,
+            from_='2020-10-31T00:00:00-04:00',  # DST change happens on 2020-11-01 02:00
+            until='2020-11-02T03:00:00-05:00',
+            interval='hour',
+            day_start_time='00:00',
+            day_end_time='03:00',
+            week_days='2,3,4,5,7',
+            timezone='America/Montreal',
+        )
+
+        assert_that(results, has_entries(total=equal_to(4)))
+        common_fields = {
+            'tenant_uuid': MASTER_TENANT,
+            'agent_id': 42,
+            'agent_number': '1001',
+        }
+        assert_that(
+            results['items'],
+            has_items(
+                has_entries(
+                    **common_fields,
+                    **{'from': '2020-11-01T00:00:00-04:00'},
+                    until='2020-11-01T01:00:00-05:00',
+                    login_time=timedelta(minutes=52 + 53).total_seconds(),
+                ),
+                has_entries(
+                    **common_fields,
+                    **{'from': '2020-11-01T01:00:00-05:00'},
+                    until='2020-11-01T02:00:00-05:00',
+                    login_time=timedelta(minutes=54).total_seconds(),
+                ),
+                has_entries(
+                    **common_fields,
+                    **{'from': '2020-11-01T02:00:00-05:00'},
+                    until='2020-11-01T03:00:00-05:00',
+                    login_time=timedelta(minutes=55).total_seconds(),
+                ),
+                has_entries(
+                    **common_fields,
+                    **{'from': '2020-10-31T00:00:00-04:00'},
+                    until='2020-11-02T03:00:00-05:00',
+                    login_time=timedelta(
+                        minutes=52 + 53 + 54 + 55 + 56
+                    ).total_seconds(),
                 ),
             ),
         )
