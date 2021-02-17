4b58e7305a28c2921a2b8b666a99b9438e1b0e69
Alexandre Fournier <afournier@wazo.io>
2021-02-17 15:02:00 -0500

    tests: add a way to set user for file creation

--- integration_tests/suite/helpers/filesystem.py
+++ integration_tests/suite/helpers/filesystem.py
@@ -1,13 +1,19 @@
 # Copyright 2021 The Wazo Authors  (see the AUTHORS file)
 # SPDX-License-Identifier: GPL-3.0-or-later
 
+FILE_USER = 'wazo-call-logd'
+FILE_GROUP = 'wazo-call-logd'
+
 
 class FileSystemClient:
     def __init__(self, execute):
         self.execute = execute
 
-    def create_file(self, path, content='content', mode='666'):
+    def create_file(self, path, content='content', mode='666', root=False):
         command = ['sh', '-c', f'echo -n {content} > {path}']
         self.execute(command)
         command = ['chmod', mode, path]
+        self.execute(command)
+        if not root:
+            command = ['chown', f'{FILE_USER}:{FILE_GROUP}', path]
         self.execute(command)
