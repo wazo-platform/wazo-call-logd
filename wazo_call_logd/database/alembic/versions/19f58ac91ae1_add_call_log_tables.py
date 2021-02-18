42036d5fd7179dba79b8c6bc561b01794d84771b
Francois Blackburn <fblackburn@wazo.community>
2021-02-18 11:04:43 -0500

    import call_log/participant tables from xivo-dao
    * also create them by alembic

---
+++ wazo_call_logd/database/alembic/versions/19f58ac91ae1_add_call_log_tables.py
@@ -0,0 +1,79 @@
+"""add-call-log-tables
+
+Revision ID: 19f58ac91ae1
+Revises: 5648242a2fee
+
+"""
+
+from alembic import op
+from sqlalchemy.dialects.postgresql import ARRAY
+import sqlalchemy as sa
+
+# revision identifiers, used by Alembic.
+revision = '19f58ac91ae1'
+down_revision = '5648242a2fee'
+
+
+def upgrade():
+    op.create_table(
+        'call_logd_call_log',
+        sa.Column('id', sa.Integer, nullable=False, primary_key=True),
+        sa.Column('date', sa.DateTime(timezone=True), nullable=False),
+        sa.Column('date_answer', sa.DateTime(timezone=True)),
+        sa.Column('date_end', sa.DateTime(timezone=True)),
+        sa.Column('tenant_uuid', sa.String(36), nullable=False),
+        sa.Column('source_name', sa.String(255)),
+        sa.Column('source_exten', sa.String(255)),
+        sa.Column('source_internal_exten', sa.Text),
+        sa.Column('source_internal_context', sa.Text),
+        sa.Column('source_line_identity', sa.String(255)),
+        sa.Column('requested_name', sa.Text),
+        sa.Column('requested_exten', sa.String(255)),
+        sa.Column('requested_context', sa.String(255)),
+        sa.Column('requested_internal_exten', sa.Text),
+        sa.Column('requested_internal_context', sa.Text),
+        sa.Column('destination_name', sa.String(255)),
+        sa.Column('destination_exten', sa.String(255)),
+        sa.Column('destination_internal_exten', sa.Text),
+        sa.Column('destination_internal_context', sa.Text),
+        sa.Column('destination_line_identity', sa.String(255)),
+        sa.Column('direction', sa.String(255)),
+        sa.Column('user_field', sa.String(255)),
+    )
+    op.create_check_constraint(
+        'call_logd_call_log_direction_check',
+        'call_logd_call_log',
+        "direction IN ('inbound','internal','outbound')",
+    )
+    op.create_table(
+        'call_logd_call_log_participant',
+        sa.Column('uuid', sa.String(38), primary_key=True),
+        sa.Column('call_log_id', sa.Integer),
+        sa.Column('user_uuid', sa.tring(38), nullable=False),
+        sa.Column('line_id', sa.Integer),
+        sa.Column(
+            'role',
+            sa.Enum('source', 'destination', name='call_logd_call_log_participant_role'),
+            nullable=False,
+        ),
+        sa.Column('tags', ARRAY(sa.String(128)), nullable=False, server_default='{}'),
+        sa.Column('answered', sa.Boolean, nullable=False, server_default='false'),
+    )
+    op.create_foreign_key(
+        constraint_name='call_logd_call_log_participant_call_log_id_fkey',
+        source_table='call_logd_call_log_participant',
+        referent_table='call_logd_call_log',
+        local_cols=['call_log_id'],
+        remote_cols=['id'],
+        ondelete='CASCADE',
+    )
+    op.create_index(
+        index_name='call_logd_call_log_participant__idx__user_uuid',
+        table_name='call_logd_call_log_participant',
+        columns=['user_uuid'],
+    )
+
+
+def downgrade():
+    op.drop_table('call_logd_call_log_participant')
+    op.drop_table('call_logd_call_log')
--- wazo_call_logd/database/models.py
+++ wazo_call_logd/database/models.py
@@ -5,17 +5,147 @@ from datetime import (
     timedelta as td,
     timezone as tz,
 )
-from sqlalchemy import (
-    Column,
-    DateTime,
-    Integer,
-    Text,
-    text,
-)
+from sqlalchemy.dialects.postgresql import ARRAY
+from sqlalchemy.ext.associationproxy import association_proxy
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.ext.hybrid import hybrid_property
+from sqlalchemy.orm import relationship
+from sqlalchemy.schema import Column, CheckConstraint, ForeignKey, Index
+from sqlalchemy.sql import case, select, text
+from sqlalchemy.types import Boolean, DateTime, Enum, Integer, String, Text
 from sqlalchemy_utils import UUIDType, generic_repr
 
+from xivo_dao.alchemy.cel import CEL
+from xivo_dao.helpers.uuid import new_uuid
+
 Base = declarative_base()
+
+
+@generic_repr
+class CallLog(Base):
+
+    __tablename__ = 'call_logd_call_log'
+
+    id = Column(Integer, nullable=False, primary_key=True)
+    date = Column(DateTime(timezone=True), nullable=False)
+    date_answer = Column(DateTime(timezone=True))
+    date_end = Column(DateTime(timezone=True))
+    tenant_uuid = Column(String(36), nullable=False)
+    source_name = Column(String(255))
+    source_exten = Column(String(255))
+    source_internal_exten = Column(Text)
+    source_internal_context = Column(Text)
+    source_line_identity = Column(String(255))
+    requested_name = Column(Text)
+    requested_exten = Column(String(255))
+    requested_context = Column(String(255))
+    requested_internal_exten = Column(Text)
+    requested_internal_context = Column(Text)
+    destination_name = Column(String(255))
+    destination_exten = Column(String(255))
+    destination_internal_exten = Column(Text)
+    destination_internal_context = Column(Text)
+    destination_line_identity = Column(String(255))
+    direction = Column(String(255))
+    user_field = Column(String(255))
+
+    participants = relationship('CallLogParticipant', cascade='all,delete-orphan')
+    participant_user_uuids = association_proxy('participants', 'user_uuid')
+
+    source_participant = relationship(
+        'CallLogParticipant',
+        primaryjoin='''and_(
+            CallLogParticipant.call_log_id == CallLog.id,
+            CallLogParticipant.role == 'source'
+        )''',
+        viewonly=True,
+        uselist=False,
+    )
+    source_user_uuid = association_proxy('source_participant', 'user_uuid')
+    source_line_id = association_proxy('source_participant', 'line_id')
+
+    destination_participant = relationship(
+        'CallLogParticipant',
+        primaryjoin='''and_(
+            CallLogParticipant.call_log_id == CallLog.id,
+            CallLogParticipant.role == 'destination'
+        )''',
+        order_by='desc(CallLogParticipant.answered)',
+        viewonly=True,
+        uselist=False,
+    )
+    destination_user_uuid = association_proxy('destination_participant', 'user_uuid')
+    destination_line_id = association_proxy('destination_participant', 'line_id')
+
+    cels = relationship(CEL)
+
+    cel_ids = []
+
+    __table_args__ = (
+        CheckConstraint(
+            direction.in_(['inbound', 'internal', 'outbound']),
+            name='call_logd_call_log_direction_check',
+        ),
+    )
+
+
+class CallLogParticipant(Base):
+
+    __tablename__ = 'call_logd_call_log_participant'
+    __table_args__ = (
+        Index('call_logd_call_log_participant__idx__user_uuid', 'user_uuid'),
+    )
+
+    uuid = Column(String(38), default=new_uuid, primary_key=True)
+    call_log_id = Column(
+        Integer,
+        ForeignKey(
+            'call_logd_call_log.id',
+            name='call_logd_call_log_participant_call_log_id_fkey',
+            ondelete='CASCADE',
+        )
+    )
+    user_uuid = Column(String(38), nullable=False)
+    line_id = Column(Integer)
+    role = Column(
+        Enum(
+            'source',
+            'destination',
+            name='call_logd_call_log_participant_role',
+        ),
+        nullable=False,
+    )
+    tags = Column(ARRAY(String(128)), nullable=False, server_default='{}')
+    answered = Column(Boolean, nullable=False, server_default='false')
+
+    call_log = relationship(
+        'CallLog',
+        primaryjoin='CallLog.id == CallLogParticipant.call_log_id',
+        uselist=False,
+    )
+
+    @hybrid_property
+    def peer_exten(self):
+        if self.role == 'source':
+            return self.call_log.requested_exten
+        else:
+            return self.call_log.source_exten
+
+    @peer_exten.expression
+    def peer_exten(cls):
+        return case(
+            [
+                (
+                    cls.role == 'source',
+                    select([CallLog.requested_exten]).where(
+                        cls.call_log_id == CallLog.id
+                    ).as_scalar()
+                )
+            ],
+            else_=select([CallLog.source_exten]).where(
+                cls.call_log_id == CallLog.id
+            ).as_scalar()
+        )
 
 
 @generic_repr
