d0e3216ef0dd151230069e60c32f8d44f457c83f
Alexandre Fournier <afournier@wazo.io>
2021-05-20 13:44:53 -0400

    export: allow None for tenant_uuids in database query

--- wazo_call_logd/database/queries/export.py
+++ wazo_call_logd/database/queries/export.py
@@ -8,7 +8,7 @@ from ..models import Export
 
 
 class ExportDAO(BaseDAO):
-    def get_by_uuid(self, export_uuid, tenant_uuids):
+    def get_by_uuid(self, export_uuid, tenant_uuids=None):
         with self.new_session() as session:
             query = session.query(Export)
             query = self._apply_filters(query, {'tenant_uuids': tenant_uuids})
