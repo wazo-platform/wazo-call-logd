65beda2bfde2cdb906052f14d93ce299c802dc4f
Julien Alie <jalie@wazo.io>
2022-04-01 14:34:16 -0400

    Add headers to mock bus messages
    Why:
    
    * This is necessary now that we consume on headers
    and not routing key

--- integration_tests/suite/helpers/bus.py
+++ integration_tests/suite/helpers/bus.py
@@ -1,4 +1,4 @@
-# Copyright 2017-2021 The Wazo Authors  (see the AUTHORS file)
+# Copyright 2017-2022 The Wazo Authors  (see the AUTHORS file)
 # SPDX-License-Identifier: GPL-3.0-or-later
 
 from wazo_test_helpers.bus import BusClient
@@ -6,5 +6,11 @@ from wazo_test_helpers.bus import BusClient
 
 class CallLogBusClient(BusClient):
     def send_linkedid_end(self, linkedid):
-        payload = {'data': {'EventName': 'LINKEDID_END', 'LinkedID': linkedid}}
-        self.publish(payload, routing_key='ami.CEL')
+        payload = {
+            'data': {
+                'EventName': 'LINKEDID_END',
+                'LinkedID': linkedid,
+            },
+            'name': 'CEL',
+        }
+        self.publish(payload, headers={'name': 'CEL'}, routing_key='ami.CEL')
