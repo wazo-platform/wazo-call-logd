d7675ac08f17d370611c3a4ba3d168790e47f331
Sebastien Duthil <sduthil@wazo.community>
2022-04-21 11:18:54 -0400

    cron: more implicit commands

--- debian/cron.d
+++ debian/cron.d
@@ -2,4 +2,4 @@
 # cron jobs for wazo-call-logs
 #
 
-25 4 * * * root . /etc/profile.d/xivo_uuid.sh; /usr/bin/wazo-call-logs
+25 4 * * * root source /etc/profile.d/xivo_uuid.sh && /usr/bin/wazo-call-logs
