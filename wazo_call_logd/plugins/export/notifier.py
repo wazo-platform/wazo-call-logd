5f750e30b82842e5b54d32216f829dad898a42af
Julien Alie <jalie@wazo.io>
2022-08-16 16:16:45 -0400

    modify notifiers to use new events

--- wazo_call_logd/plugins/export/notifier.py
+++ wazo_call_logd/plugins/export/notifier.py
@@ -2,8 +2,8 @@
 # SPDX-License-Identifier: GPL-3.0-or-later
 
 from xivo_bus.resources.call_logd.events import (
-    ExportCreatedEvent,
-    ExportUpdatedEvent,
+    CallLogExportCreatedEvent,
+    CallLogExportUpdatedEvent,
 )
 from .schemas import ExportSchema
 
@@ -13,13 +13,11 @@ class ExportNotifier:
         self._bus = bus
 
     def created(self, export):
-        export_json = ExportSchema().dump(export)
-        event = ExportCreatedEvent(export_json)
-        headers = {'tenant_uuid': str(export.tenant_uuid)}
-        self._bus.publish(event, headers)
+        payload = ExportSchema().dump(export)
+        event = CallLogExportCreatedEvent(payload, export.tenant_uuid)
+        self._bus.publish(event)
 
     def updated(self, export):
-        export_json = ExportSchema().dump(export)
-        event = ExportUpdatedEvent(export_json)
-        headers = {'tenant_uuid': str(export.tenant_uuid)}
-        self._bus.publish(event, headers)
+        payload = ExportSchema().dump(export)
+        event = CallLogExportUpdatedEvent(payload, export.tenant_uuid)
+        self._bus.publish(event)
