69289bc658ae8a3d636c8fe5be7cceab07d31b66
Francois Blackburn <fblackburn@wazo.community>
2023-02-21 08:54:38 -0500

    server: handle stop in a separate thread
    why: bullseye cheroot version (8.5.2) is now waiting for the main
    serving thread to complete its current tick before stopping. If we
    execute stop in the same thread that main thread, we will have a dead
    lock.
    
    Moreover, signal handler should not execute a lot of code (or wait) and
    only set some variable/flag. So it's not that bad to use threading at
    this level.
    
    bonus: move KeyboardInterrupt handler at the same layer than sigterm to
    improve readability

--- wazo_call_logd/http_server.py
+++ wazo_call_logd/http_server.py
@@ -62,10 +62,7 @@ class HTTPServer:
         for route in http_helpers.list_routes(app):
             logger.debug(route)
 
-        try:
-            self.server.start()
-        except KeyboardInterrupt:
-            self.server.stop()
+        self.server.start()
 
     def stop(self):
         if self.server:
