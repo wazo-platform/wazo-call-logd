7d346b1216298f3537ae58bc4a281e2c0a76624a
Francois Blackburn <fblackburn@wazo.community>
2024-07-16 09:14:43 -0400

    test: refactor not-initialized tests
    why: because verify decorators have been reordered, testing this
    scenario is much more harder. The token is now verified and if wazo-auth
    is down, we will always get server unareachable. So testing with a
    invalid-service token ensure to raise this error

--- integration_tests/suite/test_config.py
+++ integration_tests/suite/test_config.py
@@ -1,4 +1,4 @@
-# Copyright 2021-2023 The Wazo Authors  (see the AUTHORS file)
+# Copyright 2021-2024 The Wazo Authors  (see the AUTHORS file)
 # SPDX-License-Identifier: GPL-3.0-or-later
 
 from hamcrest import assert_that, calling, equal_to, has_entry, has_key, has_properties
@@ -12,6 +12,8 @@ from .helpers.wait_strategy import CallLogdEverythingUpWaitStrategy
 
 
 class TestConfig(IntegrationTest):
+    wait_strategy = CallLogdEverythingUpWaitStrategy()
+
     def test_get(self):
         result = self.call_logd.config.get()
         assert_that(result, has_key('rest_api'))
@@ -29,39 +31,22 @@ class TestConfig(IntegrationTest):
                 raises(CallLogdError, has_properties(status_code=401)),
             )
 
-    def test_restrict_on_with_slow_wazo_auth(self):
-        config_file = '/etc/wazo-call-logd/conf.d/01-master-tenant.yml'
-
-        self.filesystem.create_file(
-            config_file,
-            content='auth: {master_tenant_uuid: ""}',
-        )
-        self.stop_service('auth')
-        self.restart_service('call-logd')
-        self.reset_clients()
-
+    def test_restrict_when_service_token_not_initialized(self):
         def _returns_503():
-            try:
-                assert_that(
-                    calling(self.call_logd.config.get),
-                    raises(
-                        CallLogdError,
-                        has_properties(
-                            status_code=503,
-                            error_id='not-initialized',
-                        ),
+            assert_that(
+                calling(self.call_logd.config.get),
+                raises(
+                    CallLogdError,
+                    has_properties(
+                        status_code=503,
+                        error_id='not-initialized',
                     ),
-                )
-            except ConnectionError:
-                raise AssertionError
+                ),
+            )
 
-        until.assert_(_returns_503, tries=10)
-
-        self.filesystem.remove_file(config_file)
-        self.start_service('auth')
-        self.restart_service('call-logd')
-        self.reset_clients()
-        CallLogdEverythingUpWaitStrategy().wait(self)
+        config = {'auth': {'username': 'invalid-service', 'master_tenant_uuid': ''}}
+        with self.call_logd_with_config(config):
+            until.assert_(_returns_503, tries=10)
 
     def test_update_config(self):
         debug_true_config = [
