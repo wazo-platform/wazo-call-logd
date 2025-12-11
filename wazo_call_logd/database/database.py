7ea71fdfd8d024f8b49e2f086ed8cdeb802e1c3a
Charles Langlois <charles.langlois@wazo.io>
2025-12-11 09:21:36 -0500

    sqlalchemy: use connection.execute
    and avoid other shortcuts to evaluate sql
    as per sqlalchemy 2.0

--- wazo_call_logd/database/database.py
+++ wazo_call_logd/database/database.py
@@ -1,4 +1,4 @@
-# Copyright 2021-2023 The Wazo Authors  (see the AUTHORS file)
+# Copyright 2021-2025 The Wazo Authors  (see the AUTHORS file)
 # SPDX-License-Identifier: GPL-3.0-or-later
 
 import logging
@@ -23,6 +23,7 @@ def upgrade(uri):
 
     logger.info('Upgrading database')
     engine = create_engine(uri)
-    wait_is_ready(engine)
+    with engine.connect() as connection:
+        wait_is_ready(connection)
     alembic.command.upgrade(config, 'head')
     logger.info('Database upgraded')
--- wazo_call_logd/database/queries/helper.py
+++ wazo_call_logd/database/queries/helper.py
@@ -1,4 +1,4 @@
-# Copyright 2021-2023 The Wazo Authors  (see the AUTHORS file)
+# Copyright 2021-2025 The Wazo Authors  (see the AUTHORS file)
 # SPDX-License-Identifier: GPL-3.0-or-later
 
 from contextlib import contextmanager
@@ -11,5 +11,6 @@ class HelperDAO(BaseDAO):
     @contextmanager
     def db_ready(self):
         with self.new_session() as session:
-            wait_is_ready(session)
+            connection = session.connection()
+            wait_is_ready(connection)
             yield
